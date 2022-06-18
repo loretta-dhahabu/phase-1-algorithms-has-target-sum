63c0b2b8370bc75accac7fb84622774740c8ce35
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-18 17:25:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for ( let i = 0; i < array.length; i += 1 )
+  {
+    if ( array.indexOf( target - array[ i ] ) !== -1 && array.indexOf( target - array[ i ] ) !== i )
+    {
+      return true
+    }  
+  }
+    {
+      return false
+    }
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  0(n)
+
 */
 
 /* 
   Add your pseudocode here
+
+  iterate the length of the array
+    if value of the difference of target and current value of the iterated position in array exists in the array and the position of that value is not the current iterating position
+      return true
+  else reture false
 */
 
 /*
@@ -25,7 +43,17 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log( "" );
+  
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([12, 9, 4, 21], 13));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([8, 2, 5, 13], 44));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 12, 9, 7, 21], 17));
+
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
